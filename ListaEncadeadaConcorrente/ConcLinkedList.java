a36b5dc65c80b1328686e8bacc27e4d3e33fb06e
Gustavo Carvalho <guga.a.carvalho@gmail.com>
2018-05-27 13:25:33 -0300

    Fim da lista concorrente

--- ListaEncadeadaConcorrente/ConcLinkedList.java
+++ ListaEncadeadaConcorrente/ConcLinkedList.java
@@ -1,18 +1,19 @@
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.Semaphore;
 
 public class ConcLinkedList<T> {
 	private Node<T> headNode;
 	private Node<T> tailNode;
 	private Node<T> tailSentinelNode;
 	
-	private ReentrantLock searchLock;
-	private ReentrantLock searchPrivLock;
-	private ReentrantLock insertLock;
-	private ReentrantLock removeLock;
+	private Semaphore searchLock;
+	private Semaphore searchPrivLock;
+	private Semaphore insertLock;
+	private Semaphore removeLock;
 	
-	private ReentrantLock activeSearchesLock;
+	private Semaphore activeSearchesLock;
 	private int activeSearches = 0;
-	
+	private int currentId = 1;
+
 	public class Iterator {
 		private Node<T> node;
 		
@@ -31,13 +32,20 @@ public class ConcLinkedList<T> {
 					node.getNextNode() != null);
 		}
 		
-		public void advance() throws Exception {
+		public boolean advance() {
 			if (!isValid())
-				throw new Exception("Iterador invalido");
+				return false;
+			
 			node = node.getNextNode();
 			
-			if (node == tailSentinelNode) node = null;
+			if (node == tailSentinelNode)
+				return false;
+			return true;
 		}
+	}
+	
+	public Iterator getNullIt() {
+		return new Iterator(null);
 	}
 	
 	public ConcLinkedList() {
@@ -45,160 +53,210 @@ public class ConcLinkedList<T> {
 		headNode = new Node<T>();
 		headNode.setNextNode(tailSentinelNode);
 		tailNode = headNode;
-		searchLock = new ReentrantLock();
-		insertLock = new ReentrantLock();
-		removeLock = new ReentrantLock();
-		searchPrivLock = new ReentrantLock();
-		activeSearchesLock = new ReentrantLock();
+		searchLock = new Semaphore(1);
+		insertLock = new Semaphore(1);
+		removeLock = new Semaphore(1);
+		searchPrivLock = new Semaphore(1);
+		activeSearchesLock = new Semaphore(1);
 		
 		activeSearches = 0;
+		currentId = 1;
 	}
 	
-	private void privSearch(T value, Iterator result) {
-		System.out.println("Tentando iniciar busca");
-		searchLock.lock();
-		activeSearchesLock.lock();
+	public Iterator getHead() {
+		return new Iterator(headNode.getNextNode());
+	}
+	
+	private void privSearch(T value, Iterator result, int tId) throws InterruptedException {
+		System.out.println("S-" + tId + ": Tentando iniciar busca");
+		searchLock.acquire();
+		activeSearchesLock.acquire();
 		if (activeSearches == 0) {
-			System.out.println("Tentando garantir privilégio as buscas");
-			searchPrivLock.lock();
-			System.out.println("Garantido privilégio as buscas");
+			System.out.println("S-" + tId + ": Tentando garantir privilégio as buscas");
+			searchPrivLock.acquire();
+			System.out.println("S-" + tId + ": Garantido privilégio as buscas");
 		}
+		
+		System.out.println("S-" + tId + ": Subindo buscas ativas de " + activeSearches + " para " + (activeSearches + 1));
 		activeSearches++;
-		activeSearchesLock.unlock();
-		searchLock.unlock();
-		System.out.println("Iniciando busca");
-
+		activeSearchesLock.release();
+		searchLock.release();
+		System.out.println("S-" + tId + ": Iniciando busca");
 		
 		Node<T> curr = headNode.getNextNode();
-		while (curr != null && !curr.getValue().equals(value))
+		while (curr != tailSentinelNode && !curr.getValue().equals(value))
 			curr = curr.getNextNode();
 		
-		System.out.println("Tentando reduzir contador de busca");
-		activeSearchesLock.lock();
+		System.out.println("S-" + tId + ": Tentando reduzir contador de busca");
+		activeSearchesLock.acquire();
+		System.out.println("S-" + tId + ": Reduzido contador de busca de " + activeSearches + " para " + (activeSearches - 1));
 		activeSearches--;
-		if (activeSearches == 0)
-			searchPrivLock.unlock();
-		activeSearchesLock.unlock();
-		System.out.println("Reduzido contador de busca");
+		if (activeSearches == 0) {
+			System.out.println("S-" + tId + ": Liberando privilégio de busca");
+			searchPrivLock.release();
+		}
+		activeSearchesLock.release();
 		
-		result = new Iterator(curr);
+		result.node = curr;
 	}
 	
 	public Thread getSearchT(T value, Iterator ret) {
 		return new Thread() {
+			private int id = currentId++;
+			
 			public void run(){
-				privSearch(value, ret);
+				try {
+					privSearch(value, ret, id);
+				} catch (InterruptedException e) {
+					System.out.println("Erro na thread S-" + id);
+					e.printStackTrace();
+				}
 			}
 		};
 	}
 	
-	private void privSearchNext(T value, Iterator ret) {
-		System.out.println("Tentando iniciar busca");
-		searchLock.lock();
-		activeSearchesLock.lock();
+	private void privSearchNext(T value, Iterator ret, int tId) throws InterruptedException {
+		System.out.println("S-" + tId + ": Tentando iniciar busca");
+		searchLock.acquire();
+		activeSearchesLock.acquire();
 		if (activeSearches == 0) {
-			System.out.println("Tentando garantir privilégio as buscas");
-			searchPrivLock.lock();
-			System.out.println("Garantido privilégio as buscas");
+			System.out.println("S-" + tId + ": Tentando garantir privilégio as buscas");
+			searchPrivLock.acquire();
+			System.out.println("S-" + tId + ": Garantido privilégio as buscas");
 		}
+		
+		System.out.println("S-" + tId + ": Subindo buscas ativas de " + activeSearches + " para " + (activeSearches + 1));
 		activeSearches++;
-		activeSearchesLock.unlock();
-		searchLock.unlock();
-		System.out.println("Iniciando busca");
+		activeSearchesLock.release();
+		searchLock.release();
+		System.out.println("S-" + tId + ": Iniciando busca");
 		
 		Node<T> curr = headNode;
-		while (curr.getNextNode() != null && !curr.getNextNode().getValue().equals(value))
+		while (curr.getNextNode() != tailSentinelNode && !curr.getNextNode().getValue().equals(value))
 			curr = curr.getNextNode();
 		
-		ret = null;
-		
-		System.out.println("Tentando reduzir contador de busca");
-		activeSearchesLock.lock();
+		if (curr.getNextNode() == tailSentinelNode)
+			curr = null;
+
+		System.out.println("S-" + tId + ": Tentando reduzir contador de busca");
+		activeSearchesLock.acquire();
+		System.out.println("S-" + tId + ": Reduzido contador de busca de " + activeSearches + " para " + (activeSearches - 1));
 		activeSearches--;
-		if (activeSearches == 0)
-			searchPrivLock.unlock();
-		activeSearchesLock.unlock();
-		System.out.println("Reduzido contador de busca");
+		if (activeSearches == 0) {
+			System.out.println("S-" + tId + ": Liberando privilégio de busca");
+			searchPrivLock.release();
+		}
+		activeSearchesLock.release();
 		
-		ret = new Iterator(curr);
+		ret.node = curr;
 	}
 	
-	public Thread searchNext(T value, Iterator ret) {
-		return new Thread() {
-			public void run() {
-				privSearchNext(value, ret);
-			}
-		};
-	}
-	
-	private void privInsert(T value) {
-		System.out.println("Tentando inserir");
-		insertLock.lock();
+	private void privInsert(T value, int tId) throws InterruptedException {
+		System.out.println("I-" + tId + ": Tentando inserir");
+		insertLock.acquire();
 		
-		System.out.println("Inserindo");
+		System.out.println("I-" + tId + ": Inserindo");
 		Node<T> newNode = new Node<T>(value);
+		newNode.setNextNode(tailSentinelNode);
 		tailNode.setNextNode(newNode);
 		tailNode = newNode;
 		
-		System.out.println("Liberando insert lock");
-		insertLock.unlock();
+		System.out.println("I-" + tId + ": Liberando insert lock");
+		insertLock.release();
 	}
 	
-	public Thread insert(T value) {
+	public Thread getInsertT(T value) {
 		return new Thread() {
+			private int id = currentId++;
+
 			public void run() {
-				privInsert(value);
+				try {
+					privInsert(value, id);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		};
 	}
 	
-	private void privRemoveNext(Iterator t, Boolean b) {
+	private void privRemove(T value, int tId) throws InterruptedException {
 		
-		System.out.println("Adquirindo removelock");
-		removeLock.lock();
+		System.out.println("R-" + tId + ": Adquirindo removelock");
+		removeLock.acquire();
+		
+		Iterator t = getNullIt();
+		privSearchNext(value, t, tId);
 		
 		if (t == null || !t.isValid() || t.node.getNextNode() == tailSentinelNode) {
-			b = new Boolean(false);
-			System.out.println("Falha na remoção, liberando removelock");
-			removeLock.unlock();
+			System.out.println("R-" + tId + ": Falha na remoção, liberando removelock");
+			removeLock.release();
+			return;
 		}
 		
 		Node<T> beforeTarget = t.node;
 		
-		System.out.println("Bloqueando inicio de buscas");
-		searchLock.lock();
-		System.out.println("Esperando buscas acabarem");
-		searchPrivLock.lock();
-		System.out.println("Bloqueando inicio de inserções");
-		insertLock.lock();
+		System.out.println("R-" + tId + ": Bloqueando inicio de buscas");
+		searchLock.acquire();
+		System.out.println("R-" + tId + ": Esperando buscas acabarem");
+		searchPrivLock.acquire();
+		System.out.println("R-" + tId + ": Bloqueando inicio de inserções");
+		insertLock.acquire();
 		
-		beforeTarget.setNextNode(beforeTarget.getNextNode().getNextNode());
+		Node<T> afterRemoved = beforeTarget.getNextNode().getNextNode();
 		beforeTarget.getNextNode().setNextNode(null);
+		beforeTarget.setNextNode(afterRemoved);
 		
-		System.out.println("Liberando locks");
-		insertLock.unlock();
-		searchPrivLock.unlock();
-		searchLock.unlock();
+		System.out.println("R-" + tId + ": Liberando locks");
+		insertLock.release();
+		searchPrivLock.release();
+		searchLock.release();
 		
-		removeLock.lock();
-		b = new Boolean(true);
+		removeLock.release();
 	}
 	
-	public Thread remove(T value, Boolean ret) {
+	public Thread getRemoveT(T value) {
 		return new Thread() {
+			private int id = currentId++;
+
 			public void run() {
-				Iterator beforeTarget = null;
-				privSearchNext(value, beforeTarget);
-				removeNext(beforeTarget, ret);
+				try {
+					privRemove(value, id);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		};
 	}
 	
-	public Thread removeNext(Iterator it, Boolean ret) {
-		return new Thread() {
-			public void run() {
-				privRemoveNext(it, ret);
-			}
-		};
-	}
+//	public Thread getRemoveNextT(Iterator it) {
+//		return new Thread() {
+//			private int id = currentId++;
+//			
+//			public void run() {
+//				try {
+//					privRemoveNext(it, id);
+//				} catch (InterruptedException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+//			}
+//		};
+//	}
+//	
+//	public Thread getSearchNextT(T value, Iterator ret) {
+//		return new Thread() {
+//			private int id = currentId++;
+//
+//			public void run() {
+//				try {
+//					privSearchNext(value, ret, id);
+//				} catch (InterruptedException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+//			}
+//		};
+//	}
 }
---
+++ ListaEncadeadaConcorrente/ListTester.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+
+public class ListTester {
+	public static void main(String[] args) {
+		ArrayList<Thread> threads = new ArrayList<>();
+		ConcLinkedList<Integer> list = new ConcLinkedList<>();
+		
+		// Inserções
+		for (int i = 1; i <= 20; i++) {
+			threads.add(list.getInsertT(new Integer(i)));
+		}
+		
+		// Buscas
+		ArrayList<ConcLinkedList<Integer>.Iterator> iterators = new ArrayList<>();
+		for (int i = 1; i <= 20; i += 2) {
+			ConcLinkedList<Integer>.Iterator ret = list.getNullIt();
+			threads.add(list.getSearchT(new Integer(i), ret));
+			iterators.add(ret);
+		}
+		
+		// Remoções
+		for (int i = 11; i <= 20; i += 2) {
+			threads.add(list.getRemoveT(new Integer(i)));
+		}
+		
+		// Lançar todas as threads
+		for (Thread t : threads) {
+			t.start();
+		}
+		
+		// Esperar por todas as threads
+		for (Thread t : threads) {
+			try {
+				t.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		System.out.println("\nIteradores de pesquisa:");
+		
+		for (ConcLinkedList<Integer>.Iterator it : iterators) {
+			try {
+				System.out.println("Valor it de busca: " + it.getValue());
+			} catch (Exception e) {
+				System.out.println("Valor it de busca: it invalido");
+			}
+		}		
+		
+		System.out.println("\nLista atual:");
+		ConcLinkedList<Integer>.Iterator it = list.getHead();
+		do {
+			try {
+				System.out.println("Valor it: " + it.getValue());
+			} catch (Exception e) {
+				System.out.println("Erro de it");
+			}
+		} while (it.advance());
+	}
+}
